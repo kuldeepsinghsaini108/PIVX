12d57bbe011a2891e390845ad988f33064c5486e
kuldeep <30282127+kuldeepsinghsaini108@users.noreply.github.com>
2017-07-25 19:47:08 +0530

    param fixes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -57,9 +57,9 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
     1500984600, // * UNIX timestamp of last checkpoint block
-    0,     * total number of transactions between genesis and last checkpoint
+    0,    // * total number of transactions between genesis and last checkpoint
                 //   (the tx=... number in the SetBestChain debug.log lines)
-     2000        // * estimated number of transactions per day after checkpoint
+     2000       // * estimated number of transactions per day after checkpoint
 };
 
 static Checkpoints::MapCheckpoints mapCheckpointsTestnet =
